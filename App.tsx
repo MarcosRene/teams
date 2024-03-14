9007ec1d10cc8317b4aef590e759a243ec1874c9
Marcos Renê <renemoura789@gmail.com>
2024-03-14 12:19:04 -0300

    feat: add fonts

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
 import { ThemeProvider } from 'styled-components'
 
 import { Groups } from '@screens/Groups'
@@ -5,6 +6,14 @@ import { Groups } from '@screens/Groups'
 import theme from './src/theme'
 
 export default function App() {
+  const [isFontsLoaded] = useFonts({
+    Roboto_400Regular, Roboto_700Bold
+  })
+
+  if (!isFontsLoaded) {
+    return null;
+  }
+  
   return (
     <ThemeProvider theme={theme}>
       <Groups />
