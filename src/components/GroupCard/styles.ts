a2f2110f4b9ee5eaf8026466b04b95ba7dce9d00
Marcos Renê <renemoura789@gmail.com>
2024-03-14 13:31:24 -0300

    feat: add GroupCard component

---
+++ src/components/GroupCard/styles.ts
@@ -0,0 +1,34 @@
+import { TouchableOpacity } from 'react-native'
+import { UsersThree } from 'phosphor-react-native'
+import styled, { css } from 'styled-components/native'
+
+export const Container = styled(TouchableOpacity)`
+  ${({ theme }) => css`
+    height: 90px;
+    width: 100%;
+    margin-bottom: 12px;
+    padding: 24px;
+
+    background-color: ${theme.COLORS.GRAY_500};
+    border-radius: 6px;
+
+    flex-direction: row;
+    align-items: center;
+  `}
+`
+
+export const Title = styled.Text`
+  ${({ theme }) => css`
+    font-size: ${theme.FONT_SIZE.MD}px;
+    font-family: ${theme.FONT_FAMILY.REGULAR}px;
+    color: ${theme.COLORS.GRAY_200};
+  `}
+`
+
+export const Icon = styled(UsersThree).attrs(({ theme }) => ({
+  size: 32,
+  color: theme.COLORS.GREEN_700,
+  weight: 'fill'
+}))`
+  margin-right: 20px;
+`
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,7 +1,8 @@
 import { Header } from '@components/Header';
+import { Highlight } from '@components/Highlight';
+import { GroupCard } from '@components/GroupCard';
 
 import { Container } from './styles';
-import { Highlight } from '@components/Highlight';
 
 export function Groups() {
   return (
@@ -12,6 +13,8 @@ export function Groups() {
         title='Turmas'
         subtitle='Jogue com a sua turma'
       />
+
+      <GroupCard title="Galera do ignite" />
     </Container>
   );
 }
