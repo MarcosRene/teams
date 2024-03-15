c37b6299c20d3e9e01078e6bf37bf201253cc4a8
Marcos Renê <renemoura789@gmail.com>
2024-03-15 18:04:39 -0300

    feat: add open group

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -21,6 +21,10 @@ export function Groups() {
     navigation.navigate('new');
   }
 
+  function handleOpenGroup(group: string) {
+    navigation.navigate('players', { group });
+  }
+
   useFocusEffect(
     useCallback(() => {
       async function fetchGroups() {
@@ -45,7 +49,9 @@ export function Groups() {
       <FlatList
         data={groups}
         keyExtractor={(item) => item}
-        renderItem={({ item }) => <GroupCard title={item} />}
+        renderItem={({ item }) => (
+          <GroupCard title={item} onPress={() => handleOpenGroup(item)} />
+        )}
         contentContainerStyle={groups.length === 0 && { flex: 1 }}
         ListEmptyComponent={() => (
           <ListEmpty message="Que tal cadastrar a primeira turma?" />
